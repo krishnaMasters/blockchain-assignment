432061a14e63ec16d9a85f320e801dfdf6deff45
krishnaMasters <2023sl93035@wilp.bits-pilani.ac.in>
2024-10-23 17:43:40 +0530

    Implemented Navbar (having a button to connect to a wallet)

--- app/layout.js
+++ app/layout.js
@@ -25,11 +25,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
         <TrackingProvider>
+          <NavBar />
           {children}
+          <Footer />
         </TrackingProvider>
-        <Footer />
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,11 +1,85 @@
-import React from 'react';
+"use client";
+import React, { useEffect, useContext, useState } from "react";
+import {
+  Table,
+  Form,
+  Services,
+  Profile,
+  CompleteShipment,
+  GetShipment,
+  StartShipment,
+} from "../Components/index";
+import { TrackingContext } from "@/Context/TrackingContext";
 
 const index = () => {
+  const {
+    currentUser,
+    createShipment,
+    getAllShipment,
+    completeShipment,
+    getShipment,
+    startShipment,
+    getShipmentCount,
+  } = useContext(TrackingContext);
+
+  // state variables
+  const [createShipmentModel, setCreateShipmentModel] = useState(false);
+  const [openProfile, setOpenProfile] = useState(false);
+  const [startModel, setStartModel] = useState(false);
+  const [completeModel, setCompleteModel] = useState(false);
+  const [getModel, setGetModel] = useState(false);
+  // data state variables
+  const [allShipmentsData, setAllShipmentsData] = useState();
+
+  useEffect(() => {
+    const getCampaignsData = getAllShipment();
+
+    return async () => {
+      const allData = await getCampaignsData;
+      setAllShipmentsData(allData);
+    };
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <Services
+        setOpenProfile={setOpenProfile}
+        setCompleteModel={setCompleteModel}
+        setGetModel={setGetModel}
+        setStartModel={setStartModel}
+      />
+      <Table
+        setCreateShipmentModel={setCreateShipmentModel}
+        allShipmentsData={allShipmentsData}
+      />
+      <Form
+        createShipmentModel={createShipmentModel}
+        createShipment={createShipment}
+        setCreateShipmentModel={setCreateShipmentModel}
+      />
+      <Profile
+        openProfile={openProfile}
+        setOpenProfile={setOpenProfile}
+        currentUser={currentUser}
+        getShipmentCount={getShipmentCount}
+      />
+      <CompleteShipment
+        completeModel={completeModel}
+        setCompleteModel={setCompleteModel}
+        completeShipment={completeShipment}
+      />
+      <GetShipment
+        getModel={getModel}
+        setGetModel={setGetModel}
+        getShipment={getShipment}
+      />
+      <StartShipment
+        startModel={startModel}
+        setStartModel={setStartModel}
+        startShipment={startShipment}
+      />
+    </>
+  );
+};
 
 export default index;
